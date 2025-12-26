88b5a64f8679048d6fb1408167053c933fc6adec
Prince Fahim Al-Araf <princefahimaa@gmail.com>
2025-12-26 12:30:00 +0600

    done with email SOS

--- backend/routes/familyRoutes.js
+++ backend/routes/familyRoutes.js
@@ -1,12 +1,13 @@
 // routes/familyRoutes.js
 import express from "express";
 import { protect } from "../middleware/auth.js";
-import { addFamilyMember, removeFamilyMember } from "../controllers/familyController.js";
+import { addFamilyMember, removeFamilyMember, sendSosAlert } from "../controllers/familyController.js";
 
 const router = express.Router();
 
 router.post("/add", protect, addFamilyMember);
 router.post("/remove",protect, removeFamilyMember);
+router.post("/sos", protect, sendSosAlert);
 
 export default router;
 
