9dbf6c5d411ae4825001cf0ea693c26d6ba093d1
Prince Fahim Al-Araf <princefahimaa@gmail.com>
2026-01-18 16:19:04 +0600

    dir. structure fix

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ import { connectDB } from "./config/db.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const PORT = process.env.PORT || 5001;
 
